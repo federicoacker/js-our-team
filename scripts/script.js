94ce018a3c6484923624e9caed9a963a12efdb74
federicoacker <acker.federico@gmail.com>
2026-04-10 15:36:03 +0200

    Aggiunta una funzione che mi crea la stringa completa di html facendo il giro di tutti i teamMembers e alla fine me la mette nell'innerHTML del mio container dove li voglio

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
+const teamContainer = document.querySelector(".team-container>.row")
+
 function createElement(teamMember){
     const tmpString = `
     <div class="team-entry col-8 offset-2 offset-md-0 col-md-6 col-lg-4">
         <div class="team-card row g-0">
             <div class="col-12 col-xxl-5 d-flex p-2 align-items-center justify-content-center">
                 <div class="team-card-image">
-                    <img class="img-fluid" src="${teamMember.img}" alt="${teamMember.name}">
+                        <img class="img-fluid" src="${teamMember.img}" alt="${teamMember.name}">
                 </div>
             </div>
             <div class="team-card-body col-12 col-xxl-7 d-flex flex-column justify-content-center text-center text-xxl-start row-gap-2 px-3 py-2">
@@ -13,5 +15,18 @@ function createElement(teamMember){
                 <a href="mailto:${teamMember.email}" class="card-email">${teamMember.email}</a>
             </div>
         </div>
-    </div>`
+    </div>
+    `
+
+    return tmpString;
 }
+
+function renderTeam(){
+    let tmpString = ``;
+    for(member of teamMembers){
+        tmpString += createElement(member);
+    }
+    teamContainer.innerHTML = tmpString;
+}
+
+renderTeam();
